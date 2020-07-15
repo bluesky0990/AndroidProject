8a572853a1780e6656f570895f2d49beb872b8d0
HG <bluesky0990@naver.com>
2020-07-15 17:05:28 +0900

    Add Design

--- app/src/main/java/com/nellem/practice04/login/Login.java
+++ app/src/main/java/com/nellem/practice04/login/Login.java
@@ -22,6 +22,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import static android.content.ContentValues.TAG;
 
